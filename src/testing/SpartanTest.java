a275f62ce5fb9aab0dc8a3e8c9ad247650b83154
tyopoyt <crousedb@dukes.jmu.edu>
2018-05-04 22:45:08 -0400

    Add a test that fails for demonstration.

--- src/testing/SpartanTest.java
+++ src/testing/SpartanTest.java
@@ -1,10 +1,18 @@
 package testing;
 
 import org.junit.Before;
+import org.junit.Test;
+
+import static junit.framework.TestCase.fail;
 
 public class SpartanTest {
 
   @Before
   public void setUp() {
+  }
+
+  @Test
+  public void testConstructors() {
+    fail();
   }
 }
