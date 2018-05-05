72dedb03d1c91ade1eefd385aee3879d6a321e0d
Thomas Campbell <trc527@gmail.com>
2018-05-05 13:31:36 -0400

    Create NormalRoomTest

---
+++ src/testing/environmentsTesting/NormalRoomTest.java
@@ -0,0 +1,16 @@
+package testing.environmentsTesting;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class NormalRoomTest {
+
+  @Before
+  public void setUp(){
+  }
+
+  @Test
+
+}
