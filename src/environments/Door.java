9c839e7ad5f9c0a575cc58529c0653dd1ba1ce26
tyopoyt <crousedb@dukes.jmu.edu>
2018-05-05 13:11:44 -0400

    Implement Door class.

--- src/environments/Door.java
+++ src/environments/Door.java
@@ -1,4 +1,37 @@
 package environments;
 
+import items.Key;
+
 public class Door {
+  private Key key;
+  private boolean isLocked;
+
+  /**
+   * 1-arg constructor for a Door.
+   *
+   * @param key the key that unlocks this door
+   */
+  public Door(Key key) {
+    this.key = key;
+    isLocked = true;
+  }
+
+  /**
+   * Attempt to unlock the door.
+   *
+   * @param key the key we are attempting to unlock with
+   * @return whether the door was successfully unlocked
+   */
+  public boolean unlock(Key key) {
+    return isLocked = (this.key == key);
+  }
+
+  /**
+   * Check if the door is locked.
+   *
+   * @return isLocked
+   */
+  public boolean isLocked() {
+    return isLocked;
+  }
 }
