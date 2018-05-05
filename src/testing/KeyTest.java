99d759692b5f2375e3e4fdb1fb0bd10f7656fb5b
Thomas Campbell <trc527@gmail.com>
2018-05-05 13:00:45 -0400

    Test Key Class Created

---
+++ src/testing/KeyTest.java
@@ -0,0 +1,21 @@
+package testing;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class KeyTest {
+
+  @Before
+  public void setUp() throws Exception {
+  }
+
+  @Test
+  public void getCode() {
+  }
+
+  @Test
+  public void getDescription() {
+  }
+}
