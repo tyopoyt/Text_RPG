94d5fa6707c3f2aa0e6da4621cc8b4abf86b24d6
Thomas Campbell <trc527@gmail.com>
2018-05-05 13:09:16 -0400

    Create NormalRoom Class

---
+++ src/environments/NormalRoom.java
@@ -0,0 +1,10 @@
+package environments;
+
+import items.Item;
+
+public class NormalRoom extends Room {
+
+  public NormalRoom(String description, Character[] mobs, Item[] items){
+    super(description, mobs, items);
+  }
+}
