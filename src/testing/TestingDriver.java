6cc989cd4ede491269ae6adb17861410cd487ed7
tyopoyt <crousedb@dukes.jmu.edu>
2018-05-05 13:05:36 -0400

    Update TestingDriver.

--- src/testing/TestingDriver.java
+++ src/testing/TestingDriver.java
@@ -4,6 +4,9 @@ import items.Key;
 
 public class TestingDriver {
   public static void main(String[] args) {
-    //Key key0 = new Key();
+    Key key0 = new Key("A key.");
+    Key key1 = key0;
+
+    System.out.println(key0 == key1);
   }
 }
