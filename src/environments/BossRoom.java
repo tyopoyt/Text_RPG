be49c7282e5ba66a0e282ea659fe935c1f161427
Thomas Campbell <trc527@gmail.com>
2018-05-05 13:25:45 -0400

    Edit BossRoom

--- src/environments/BossRoom.java
+++ src/environments/BossRoom.java
@@ -5,6 +5,14 @@ import characters.Character;
 
 public class BossRoom extends Room {
 
+  /**
+   * Constructor for Boss Room. NOTE: THE BOSS ROOM SHOULD ONLY HAVE ONE ENEMY, UNLESS THE BOSS IS CAPABLE OF SUMMONING ADS
+   *
+   * @param description SEE SUPER
+   * @param mobs SEE SUPER
+   * @param items SEE SUPER
+   * @param doors SEE SUPER
+   */
   public BossRoom(String description, Character[] mobs, Item[] items, Door[] doors){
     super(description, mobs, items, doors);
   }
