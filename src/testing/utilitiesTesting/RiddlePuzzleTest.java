40c3756364df17bbe803ea333a65f45f40a46bc5
tyopoyt <crousedb@dukes.jmu.edu>
2018-05-05 22:50:02 -0400

    Make Puzzle's giveUp method take a characters.Character argument

--- src/testing/utilitiesTesting/RiddlePuzzleTest.java
+++ src/testing/utilitiesTesting/RiddlePuzzleTest.java
@@ -13,12 +13,20 @@ public class RiddlePuzzleTest {
 
   @Before
   public void setUp() {
-    Riddle r = new Riddle("This is a riddle?", "Yes");
+    Riddle r = new Riddle("This is a riddle?", "Yes that's correct");
     rp = new RiddlePuzzle("A RiddlePuzzle", r);
   }
 
   @Test
   public void testAttempt() {
+    rp.setInput("incorrect");
+    assertFalse(rp.attempt());
+    rp.setInput("yes");
+    assertTrue(rp.attempt());
+    rp.setInput("correct");
+    assertTrue(rp.attempt());
+    rp.setInput("yes 987865765 correct");
+    assertTrue(rp.attempt());
   }
 
   @Test
--- src/utilities/RiddlePuzzle.java
+++ src/utilities/RiddlePuzzle.java
@@ -1,11 +1,30 @@
 package utilities;
 
+/**
+ * A puzzle involving a riddle.
+ */
 public class RiddlePuzzle extends Puzzle{
   private Riddle riddle;
   private String[] answers;
+  private String input;
 
+  /**
+   * 2-arg constructor.
+   *
+   * @param description a description of the RiddlePuzzle
+   * @param riddle the riddle
+   */
   public RiddlePuzzle(String description, Riddle riddle){
     super(description);
+  }
+
+  /**
+   * Mutator for input.
+   *
+   * @param input the user's input
+   */
+  public void setInput(String input) {
+
   }
 
   @Override
@@ -14,7 +33,7 @@ public class RiddlePuzzle extends Puzzle{
   }
 
   @Override
-  public void giveUp(){
+  public void giveUp(Character actor){
 
   }
 }
