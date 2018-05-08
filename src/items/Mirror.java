2fa9cd99fe9ebbb947b09f674e19fd37b00f4b96
Thomas Campbell <trc527@gmail.com>
2018-05-08 16:32:02 -0400

    Create Mirror methods

--- src/items/Mirror.java
+++ src/items/Mirror.java
@@ -1,13 +1,25 @@
 package items;
 
-import characters.Player;
-
+/**
+ * Class for the Mirror Item
+ */
 public class Mirror extends Item {
 
+  /**
+   * Constructor for a mirror.
+   *
+   * @param name the name of the mirror
+   * @param description the description of the mirror
+   */
   public Mirror(String name, String description){
     super(name, 100, description, false, false, false);
   }
 
+  /**
+   * Method to look at the mirror.
+   *
+   * @return the string description of the reflection
+   */
   public String lookAt(){
     return "It's you.";
   }
