398da19f5168ad2a13cb79db150291c842b8e5a3
Thomas Campbell <trc527@gmail.com>
2018-05-09 16:27:10 -0400

    Create basic GUI

--- src/Driver.java
+++ src/Driver.java
@@ -1,2 +1,13 @@
+import GUI.GameWindow;
+
 public class Driver {
+
+  /**
+   * Driver for GAME_NAME.
+   *
+   * @param args not used
+   */
+  public static void main(String[] args) {
+    GameWindow window = new GameWindow();
+  }
 }
--- src/GUI/GameWindow.java
+++ src/GUI/GameWindow.java
@@ -1,6 +1,50 @@
 package GUI;
 
 import javax.swing.*;
+import javax.swing.border.TitledBorder;
+import java.awt.*;
 
 public class GameWindow extends JFrame {
+
+  //static Toolkit tk = Toolkit.getDefaultToolkit();
+  private static Dimension screen = Toolkit.getDefaultToolkit().getScreenSize();
+
+  public GameWindow () {
+    setLayout(new BorderLayout());
+
+    JTextArea console;
+    TitledBorder consoleBorder;
+
+    JTextArea input;
+    TitledBorder inputBorder;
+
+    //Initialize console text area
+    console = new JTextArea();
+    console.setPreferredSize(new Dimension(screen.width / 2, screen.height / 4));
+    console.setEditable(false);
+
+    consoleBorder = new TitledBorder("Console:");
+    consoleBorder.setTitleColor(Color.WHITE);
+
+    console.setBorder(consoleBorder);
+    console.setBackground(Color.BLACK);
+
+    //Initialize input text area
+    input = new JTextArea();
+    input.setPreferredSize(new Dimension(screen.width / 2, screen.height / 6));
+
+    inputBorder = new TitledBorder("Input:");
+    inputBorder.setTitleColor(Color.WHITE);
+
+    input.setBorder(inputBorder);
+    input.setBackground(Color.BLACK);
+
+    add(console, BorderLayout.NORTH);
+    add(input, BorderLayout.SOUTH);
+
+    setLocation((screen.width / 2) - (screen.width / 4), (screen.height / 2) - (int)(screen.height * (5 / 24.0)));
+    setDefaultCloseOperation(WindowConstants.EXIT_ON_CLOSE);
+    pack();
+    setVisible(true);
+  }
 }
